0f2c3bb7edea570cceb36c3c037a68888f22e294
Trevor Lesley <tlesley96@gmail.com>
2021-06-18 08:21:57 -0400

    Created list format.

---
+++ components/imagelist.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+
+function ImageList(props) {
+
+  const images = props.images.map((image) => (
+    <li key={image.id}>
+      <img src={image.url} alt={image.description}/>
+      <p>{image.description}</p>
+    </li>
+  ));
+
+  return(
+    <ul>{images}</ul>
+  )
+}
+
+export default ImageList
